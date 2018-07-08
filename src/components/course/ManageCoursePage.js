10bd0fa095376e7880e75e24518d8ee4daabc6f6
Dita Rahmane <dita.rahmane.silova@inbox.lv>
2018-07-08 17:46:37 +0300

    Change the name of button when data get saved

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -9,7 +9,8 @@ class ManageCoursePage extends Component {
         super(props, context); 
         this.state = {
             course: Object.assign({}, this.props.course),
-            errors: {}
+            errors: {},
+            saving: false
         };
 
         this.updateCourseState = this.updateCourseState.bind(this);
@@ -34,7 +35,13 @@ class ManageCoursePage extends Component {
 
     saveCourse(event) {
         event.preventDefault();
-        this.props.actions.saveCourse(this.state.course);
+        this.setState({saving: true});
+        this.props.actions.saveCourse(this.state.course)
+            .then(() => this.redirect());
+    }
+
+    redirect() {
+        this.setState({ saving: false });
         this.context.router.push('/courses');
     }
 
@@ -46,6 +53,7 @@ class ManageCoursePage extends Component {
                 onSave={this.saveCourse}
                 course={this.state.course} 
                 errors={this.state.errors}
+                saving={this.state.saving}
                 />
         );
     }
